453fb44197e6616b8f73a8718c8f5e47ac0bbc75
ArtiomOn <artiom.carter.03@mail.ru>
2021-08-11 09:44:23 +0300

    Added bd and updated

--- config/main.py
+++ config/main.py
@@ -1,17 +1,22 @@
 import logging
 import os
+from datetime import datetime
 
 import dotenv
 from aiogram import types, Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.utils import executor
+from sqlalchemy.orm import sessionmaker
 from fuzzywuzzy import fuzz
 
+from config.models import PullData, database_dsn
 from services.converter import convert_ogg_to_wav
 from services.recognizer import audio_file_to_text
 from services.storage import generate_unique_destinations
 
 logging.basicConfig(level=logging.INFO)
+
+session = sessionmaker(bind=database_dsn)()
 
 dotenv.load_dotenv()
 
@@ -35,13 +40,17 @@ async def assist(message: types.Message):
         await message.voice.download(destination=ogg_destination)
         convert_ogg_to_wav(ogg_destination, wav_destination)
         query = audio_file_to_text(wav_destination)
-        await bot.delete_message(message_id=message.message_id, chat_id=message.chat.id)
-        await reformat_text(query, message)
+        try:
+            await bot.delete_message(message_id=message.message_id, chat_id=message.chat.id)
+        except Exception as e:
+            logging.info(f'Error occurs {e} with user {message.from_user.id}')
+        else:
+            await reformat_text(query, message)
 
 
 async def reformat_text(text, message: types.Message):
     pull_choice_data = []
-    command_create_pull_data = text.partition('бот создай опрос')[1]
+    command_create_pull_data = text.partition('создай опрос')[1]
     question_first_index = text.find('опрос')
     question_last_index = text.find('выбор')
     pull_question_row = text[question_first_index: question_last_index]
@@ -53,6 +62,13 @@ async def reformat_text(text, message: types.Message):
         pull_choice_data.append(
             ' '.join(pull_choice_data_row.split()).split('выбор', int(i + 2))[int(i + 1)].capitalize())
 
+    query = PullData(user_id=message.from_user.id,
+                     pull_question=pull_question_data,
+                     pull_choice=pull_choice_data,
+                     created_at=datetime.now(),
+                     )
+    session.add(query)
+    session.commit()
     await execute_cmd(message, command_create_pull_data, pull_question_data, pull_choice_data)
 
 
--- config/settings.py
+++ config/settings.py
@@ -8,3 +8,9 @@ TOKEN = os.getenv('TOKEN')
 BASE_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STORAGE_ROOT = os.path.join(os.path.join(BASE_ROOT), 'storage')
+
+database = os.getenv('DATA_BASE')
+user = os.getenv('POSTGRES_USER')
+password = os.getenv('POSTGRES_PASSWORD')
+host = os.getenv('POSTGRES_HOST')
+port = os.getenv('POSTGRES_PORT')
