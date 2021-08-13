3551c77ad2624177ce584cc304c88a38882314fd
ArtiomOn <artiom.carter.03@mail.ru>
2021-08-13 11:44:43 +0300

    Removed postgres Updated requirements.txt Updated main.py

--- config/main.py
+++ config/main.py
@@ -1,31 +1,26 @@
 import logging
 import os
-import requests
-import shutil
-from datetime import datetime
 
 import dotenv
+import requests
 from aiogram import types, Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.utils import executor
 from aiogram.utils.exceptions import BotBlocked
-from fuzzywuzzy import fuzz
-from sqlalchemy.orm import sessionmaker
 from youtubesearchpython import *
 
-from config.models import PullData, database_dsn
 from services.converter import convert_ogg_to_wav
 from services.recognizer import audio_file_to_text
 from services.storage import generate_unique_destinations
 
 logging.basicConfig(level=logging.INFO)
 
-session = sessionmaker(bind=database_dsn)()
 
 dotenv.load_dotenv()
 
 bot = Bot(token=os.getenv('TOKEN'))
 OWM_KEY = os.getenv('OWM_KEY')
+
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
 
@@ -44,8 +39,8 @@ async def command_help(message: types.Message):
                                             '--[обязательный ответ пользователя]\n\n'
                                             'Чтобы создать опрос надо произнести ключевые слова - '
                                             'они подмечены жирным шрифтом.\n'
-                                            '<b>*Бот создай {анонимный} опрос</b> [ваш вопрос] <b>выбор</b> '
-                                            '[ваш вариант ответа], <b>выбор</b> [ваш вариант ответа]...\n\n'
+                                            '<b>*Бот создай {анонимный} опрос</b> [ваш вопрос] <b>вариант</b> '
+                                            '[ваш вариант ответа], <b>вариант</b> [ваш вариант ответа]...\n\n'
                                             'Чтобы найти видео в ютубе надо произнести ключевые слова -\n'
                                             '<b>*Бот найди видео</b> [название видео]\n\n'
                                             'Чтобы посмотреть актуальную на данный момент погоду надо произнести '
@@ -67,60 +62,49 @@ async def assist(message: types.Message):
         except Exception as e:
             logging.info(f'Error occurs {e} with user {message.from_user.id}')
         else:
-            await command_handler(query, message)
+            await command_handler(message, query)
 
 
-async def command_handler(query, message: types.Message):
-    if query.find('создай') != -1 or query.find('опрос') != -1:
-        await create_poll(query, message)
-    elif query.find('найди') != -1 or query.find('видео') != -1:
-        await get_video_link(query, message)
+async def command_handler(message: types.Message, query):
+    if (query.find('создай') or query.find('опрос')) != -1:
+        await create_poll(message, query)
+    elif (query.find('найди') or query.find('видео')) != -1:
+        await get_video_link(message, query)
     elif query.find('погода') != -1:
-        await get_weather(query, message)
+        await get_weather(message, query)
     else:
-        await bot.send_message(message.chat.id, 'Не распознал вашу команду - для информации напишите /start')
+        await bot.send_message(message.chat.id, 'Не распознал вашу команду - для информации напишите /help')
 
 
-async def create_poll(text, message: types.Message):
+async def create_poll(message: types.Message, text):
     pull_choice_data_row = []
-    # Get create poll command
-    command_create_pull_first_index = text.find('создай')
-    command_create_pull_last_index = text.find('опрос')
-    command_create_pull_data_row = text[command_create_pull_first_index: command_create_pull_last_index]
-    command_create_pull_data = ' '.join(command_create_pull_data_row.partition('создай')[2].split())
-    if fuzz.partial_ratio(command_create_pull_data, "анонимный") > 70:
+    # Get poll command
+    if text.find('анонимный') != -1:
         command_create_pull_data = 'анонимный'
-    elif command_create_pull_last_index == -1 or command_create_pull_first_index == -1:
-        command_create_pull_data = 'NonType'
     else:
         command_create_pull_data = 'обычный'
 
     # Get pull question
     question_first_index = text.find('опрос')
-    question_last_index = text.find('выбор')
+    if text.find('вариант') != -1:
+        question_last_index = text.find('вариант')
+    else:
+        question_last_index = len(text)
     pull_question_row = text[question_first_index: question_last_index]
     pull_question_data = ' '.join(pull_question_row.partition('опрос')[2].split()).capitalize()
 
     # Get poll choice
-    pull_choice_first_index = text.find('выбор')
+    pull_choice_first_index = text.find('вариант')
     pull_choice_last_index = len(text)
     pull_choice_data_words = text[pull_choice_first_index:pull_choice_last_index]
-    for i in range(pull_choice_data_words.count('выбор')):
+    for i in range(pull_choice_data_words.count('вариант')):
         pull_choice_data_row.append(
-            ''.join(pull_choice_data_words.split()).split('выбор', int(i + 2))[int(i + 1)].capitalize())
+            ''.join(pull_choice_data_words.split()).split('вариант', int(i + 2))[int(i + 1)].capitalize())
     pull_choice_data = [choices for choices in pull_choice_data_row if choices.strip()]
-    # Save data in postgres
-    query = PullData(user_id=message.from_user.id,
-                     pull_question=pull_question_data,
-                     pull_choice=pull_choice_data,
-                     created_at=datetime.now(),
-                     )
-    session.add(query)
-    session.commit()
     await poll_handler(message, command_create_pull_data, pull_question_data, pull_choice_data)
 
 
-async def get_video_link(query, message: types.Message):
+async def get_video_link(message: types.Message, query):
     command_find_video_name_first_index = query.find('видео')
     command_find_video_name_last_index = len(query)
     command_find_video_data_row = query[command_find_video_name_first_index: command_find_video_name_last_index]
@@ -128,7 +112,7 @@ async def get_video_link(query, message: types.Message):
     await get_video_handler(message, command_find_video_data)
 
 
-async def get_weather(query, message: types.Message):
+async def get_weather(message: types.Message, query):
     command_find_weather_first_index = query.find('погода')
     command_find_weather_last_index = len(query)
     command_find_weather_data_row = query[command_find_weather_first_index:command_find_weather_last_index]
@@ -138,9 +122,11 @@ async def get_weather(query, message: types.Message):
     elif command_find_weather_data_row.find('стране') > -1:
         command_find_weather_data = command_find_weather_data_row.partition('стране')[2]
         await get_weather_handler(message, command_find_weather_data.strip())
+    else:
+        await bot.send_message(message.chat.id, 'Не распознал страну, попробуйте еще раз.')
 
 
-async def poll_handler(message, command, question, choice):
+async def poll_handler(message: types.Message, command, question, choice):
     if command == 'обычный':
         if len(choice) < 2:
             await bot.send_poll(message.chat.id, question=f'{question.capitalize()}?', options=['Да', 'Нет'],
@@ -157,17 +143,17 @@ async def poll_handler(message, command, question, choice):
             await bot.send_poll(message.chat.id, question=f'{question.capitalize()}?', options=choice,
                                 is_anonymous=True)
     else:
-        await bot.send_message(message.chat.id, 'Не понял вашу команду, повторите еще раз')
+        await bot.send_message(message.chat.id, 'Не понял вашу команду, попробуйте еще раз')
 
 
 async def get_video_handler(message: types.Message, query):
-    custom_search = CustomSearch(query=f'{query}', limit=2, searchPreferences='en')
+    custom_search = CustomSearch(query=str(query), limit=1, searchPreferences='en')
 
     if custom_search.result()['result']:
-        for i in range(2):
+        for i in range(1):
             await bot.send_message(message.chat.id, dict(custom_search.result()['result'][i]).get('link'))
     else:
-        await bot.send_message(message.chat.id, 'Видео не было найдено')
+        await bot.send_message(message.chat.id, 'Видео не было найдено, попробуйте еще раз.')
 
 
 async def get_weather_handler(message: types.Message, city):
@@ -199,9 +185,8 @@ async def get_weather_handler(message: types.Message, city):
                                                 f'Температура - {str(weather_tamp)[:2]}°C\n'
                                                 f'Влажность - {weather_humidity}%')
     else:
-        await bot.send_message(message.chat.id, 'Я не нашел странну, пример ввода страны - МолдовА, РоссиЯ...')
+        await bot.send_message(message.chat.id, 'Я не нашел страну, пример ввода страны - МолдовА, РоссиЯ..')
 
 
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=False, timeout=120)
-    shutil.rmtree(r'C:\Users\artio\PycharmProjects\pull_bot\storage')
--- config/settings.py
+++ config/settings.py
@@ -8,9 +8,3 @@ TOKEN = os.getenv('TOKEN')
 BASE_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STORAGE_ROOT = os.path.join(os.path.dirname(BASE_ROOT), 'storage')
-
-database = os.getenv('DATA_BASE')
-user = os.getenv('POSTGRES_USER')
-password = os.getenv('POSTGRES_PASSWORD')
-host = os.getenv('POSTGRES_HOST')
-port = os.getenv('POSTGRES_PORT')
